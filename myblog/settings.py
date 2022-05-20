30ecd0f8f7d2dd258527f8191c7106abc2058f08
MartinaB91 <martinabjorlin@hotmail.com>
2022-05-20 07:41:44 +0000

    chore: Renamed home.html to index.html and changed related settings

--- myblog/settings.py
+++ myblog/settings.py
@@ -65,8 +65,8 @@ INSTALLED_APPS = [
 
 SITE_ID = 1
 
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/home'
+LOGIN_REDIRECT_URL = '/' #  TODO: Temporary. Change this to personal blog page or profile when created
+LOGOUT_REDIRECT_URL = '/'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
