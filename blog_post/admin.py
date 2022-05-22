2320555ba8fb6305b5f0bd1abd3f7ae3266be803
MartinaB91 <martinabjorlin@hotmail.com>
2022-05-22 12:01:16 +0000

    feat: Add classes to admin.py to controll what admin can see/do

--- blog_post/admin.py
+++ blog_post/admin.py
@@ -1,8 +1,77 @@
 from django.contrib import admin
-from .models import Category, Post, Comment, Like
+from .models import Category, Post, Comment
 
 
-admin.site.register(Post)
-admin.site.register(Category)
-admin.site.register(Comment)
-admin.site.register(Like)
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """This class is used for controlling what the Admin can see and do with posts"""
+    model = Post
+
+    list_display = (
+        'id',
+        'author',
+        'title',
+        'category',
+        'slug',
+        'published',
+        'publish_date',
+
+    )
+
+    list_filter = (
+        'author',
+        'category',
+        'published',
+        'publish_date',
+    )
+
+    list_editable = (
+        'title',
+        'category',
+        'publish_date',
+        'published',
+    )
+
+    search_fields = (
+        'title',
+        'slug',
+        'author',
+        'content',
+        'category',
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    """This class is used for controlling what the Admin can see and do with categorys"""
+    model = Category
+
+    list_display = ('name', 'slug')
+    list_filter = ('name',)
+    search_fields = ('name', 'slug')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    """This class is used for controlling what the Admin can see and do with comments"""
+    model = Comment
+
+    list_display = (
+        'blog_post',
+        'content',
+        'author',
+        'created_on',
+        'approved',
+
+        )
+    
+    list_filter = (
+        'blog_post',
+        'author',
+        'created_on',
+        'approved',
+    )
+
+    list_editable = ('content', 'approved',)
+    search_fields = ('author', 'created_on',)
+
