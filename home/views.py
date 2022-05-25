230caee398f5c16d85797fb5a7e3edae9a3b103d
MartinaB91 <martinabjorlin@hotmail.com>
2022-05-25 15:16:06 +0000

    feat: Add class HomeView to show posts on index page

--- home/views.py
+++ home/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from blog_post.models import Post
 
 
-def index(request):
-    return render(request, 'index.html')
-
+class HomeView(ListView):
+    """ 
+    Used for showing posts on homepage
+    """
+    model = Post
+    template_name = 'index.html'
