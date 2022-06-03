3c7d4e223304d5bdaae43a836684d7eee4d3093c
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-03 08:25:16 +0000

    feat: Add queryset to HomeView class to filter blogpost when page first loaded

--- home/views.py
+++ home/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView
 from blog_post.models import Post
+
 
 
 class HomeView(ListView):
     """ 
-    Used for showing posts on homepage
-    """
+     Used for showing posts on homepage
+     """
     model = Post
     template_name = 'index.html'
+    queryset = Post.objects.filter(published=1).order_by('-created_on')
