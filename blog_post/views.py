38f6f2ff8124089d368336cd372e54d8b0c6b945
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-03 15:02:03 +0000

    feat: Add class afor like/unlike and made some small changes

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,12 +1,13 @@
 from django.shortcuts import render, get_object_or_404, reverse
 from django.urls import reverse
 from django.http import HttpResponseRedirect
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView, View
 from django.views import generic
 from .models import Post
 
 
-class BlogPostList(generic.ListView): # TODO: Maybe remove later on, maybe doing the same thing as HomeView
+
+class BlogPostList(generic.ListView):  # TODO: Maybe remove later on, maybe doing the same thing as HomeView
     model = Post
     template_name = 'index.html'
     queryset = Post.objects.filter(published=1).order_by('-created_on')
@@ -26,6 +27,17 @@ class BlogPostDetail(generic.DetailView):
             request,
             "blog_post.html",
             {
-                "post": post,
+                'post': post,
+                'liked': liked,
             },
         )
+
+class BlogPostLike(View):
+    def post(self, request, slug):
+        post = get_object_or_404(Post, id=request.POST.get('post_id'))
+        if post.likes.filter(id=request.user.profile.id).exists():
+            post.likes.remove(request.user.profile)
+        else:
+            post.likes.add(request.user.profile)
+        
+        return HttpResponseRedirect(reverse('blog_post', args=[slug]))
