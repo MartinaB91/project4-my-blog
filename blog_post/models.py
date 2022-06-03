b6777840d735861f8e81f8fde4a99302acede62d
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-03 14:55:04 +0000

    feat: Add functito count number of likes

--- blog_post/models.py
+++ blog_post/models.py
@@ -78,6 +78,9 @@ class Post(models.Model):
 
     def __str__(self):
         return f'{self.author}: {self.title}'
+
+    def number_of_likes(self):
+        return self.likes.count()
   
 
 class Comment(models.Model):
