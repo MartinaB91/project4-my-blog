10a8fdf0c8dc695014b9ef1f4627870201993ce6
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-03 14:57:57 +0000

    refactor: Changed name and added views to BlogPostLike path

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import BlogPostList, BlogPostDetail
+from .views import BlogPostList, BlogPostDetail, BlogPostLike
 from . import views
 
 urlpatterns = [
     path('', views.BlogPostList.as_view(), name='index'),
     path('<slug:slug>/', views.BlogPostDetail.as_view(), name='blog_post'),
-    # path('like/<slug:slug>', BlogPostLike.as_view(), name='blog_post')
+    path('like/<slug:slug>/', views.BlogPostLike.as_view(), name='post_likes'),
+
+
 ]
