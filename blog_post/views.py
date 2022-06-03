3e536d5bc0437a52651363efb0fd254cd1abc7eb
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-03 08:23:38 +0000

    feat: Add classes to filter out published posts and when post is created.

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,8 +1,31 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, reverse
+from django.urls import reverse
+from django.http import HttpResponseRedirect
 from django.views.generic import DetailView
+from django.views import generic
 from .models import Post
 
 
-class BlogPostView(DetailView):
+class BlogPostList(generic.ListView): # TODO: Maybe remove later on, maybe doing the same thing as HomeView
     model = Post
-    template_name ='blog_post.html'
+    template_name = 'index.html'
+    queryset = Post.objects.filter(published=1).order_by('-created_on')
+
+
+class BlogPostDetail(generic.DetailView):
+
+    def get(self, request, slug, *args, **kwargs):
+        template_name = 'blog_post.html'
+        queryset = Post.objects.filter(published=1).order_by('-created_on')
+
+        post = get_object_or_404(queryset, slug=slug)
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+        return render(
+            request,
+            "blog_post.html",
+            {
+                "post": post,
+            },
+        )
