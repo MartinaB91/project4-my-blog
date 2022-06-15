570b6295663a8eb17405a3a0e485ef24ea31a165
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-15 14:39:57 +0000

    Add js functionality for display/hide comment section

--- static/js/comments.js
+++ static/js/comments.js
@@ -1,1 +1,11 @@
-
+// When user click comment button icon, comment field will be displayed/hidden.
+$(document).ready(function () {
+  $('.btn-comments').click(function () {
+    let commentWrapper = $(this).parent().parent().parent().siblings().first();
+    if (commentWrapper.hasClass('collapse')) {
+      commentWrapper.removeClass('collapse');
+    } else {
+      commentWrapper.addClass('collapse');
+    }
+  });
+});
