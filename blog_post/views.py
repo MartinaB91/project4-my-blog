4ae8abc0b03967d39e375b22d8a56ad94bd9d774
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-15 08:34:41 +0000

    feat: Add function for liking moved BlogPostList to home view

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,17 +1,36 @@
-from django.shortcuts import render, get_object_or_404, reverse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.urls import reverse
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse
 from django.views.generic import DetailView, ListView, View
 from django.views import generic
-from .models import Post
+from .models import Post, Like
 
 
+# class BlogPostList(generic.ListView):  # TODO: Maybe remove later on, maybe doing the same thing as HomeView
+#     model = Post
+#     template_name = 'index.html'
+#     queryset = Post.objects.filter(published=1).order_by('-created_on')
 
-class BlogPostList(generic.ListView):  # TODO: Maybe remove later on, maybe doing the same thing as HomeView
-    model = Post
-    template_name = 'index.html'
-    queryset = Post.objects.filter(published=1).order_by('-created_on')
+# class BlogPostList(generic.DetailView):
 
+#     def get(self, request, slug, *args, **kwargs):
+#         template_name = 'index.html'
+#         queryset = Post.objects.filter(published=1).order_by('-created_on')
+
+#         post = get_object_or_404(queryset, slug=slug)
+#         liked = False
+#         if post.likes.filter(id=request.user.id).exists():
+#             liked = True
+#         return render(
+#             request,
+#             "index.html",
+#             {
+#                 'post': post,
+#                 'liked': liked,
+#             },
+#         )
+
+    
 
 class BlogPostDetail(generic.DetailView):
 
@@ -21,8 +40,9 @@ class BlogPostDetail(generic.DetailView):
 
         post = get_object_or_404(queryset, slug=slug)
         liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
+        if post.likes.filter(id=request.user.id).exists():
             liked = True
+
         return render(
             request,
             "blog_post.html",
@@ -32,12 +52,22 @@ class BlogPostDetail(generic.DetailView):
             },
         )
 
-class BlogPostLike(View):
-    def post(self, request, slug):
-        post = get_object_or_404(Post, id=request.POST.get('post_id'))
-        if post.likes.filter(id=request.user.profile.id).exists():
-            post.likes.remove(request.user.profile)
+def like_post(request, slug):
+    user = request.user
+    if request.method == 'POST':
+        post_id = request.POST.get('post_id')
+        post_obj = get_object_or_404(Post, id=post_id)
+
+        if post_obj.likes.filter(id=request.user.id).exists():
+            post_obj.likes.remove(user.id)
+            post_obj.number_of_likes -= 1
+            post_obj.save()
+
         else:
-            post.likes.add(request.user.profile)
-        
-        return HttpResponseRedirect(reverse('blog_post', args=[slug]))
+            post_obj.likes.add(user.id)
+            post_obj.number_of_likes += 1
+            post_obj.save()
+
+    return HttpResponse(post_obj.number_of_likes)
+
+
