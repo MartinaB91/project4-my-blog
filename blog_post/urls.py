6c7de9c182da9f38b557683e1e0cb1c5ef5af7dd
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-15 08:43:47 +0000

    refactor: Changed urlpatterns to home and blogpost app

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -1,11 +1,10 @@
-from django.urls import path
-from .views import BlogPostList, BlogPostDetail, BlogPostLike
+from django.urls import path, re_path
+from .views import BlogPostDetail, like_post
 from . import views
 
+
 urlpatterns = [
-    path('', views.BlogPostList.as_view(), name='index'),
+    # path('', views.BlogPostList.as_view(), name='index'),
     path('<slug:slug>/', views.BlogPostDetail.as_view(), name='blog_post'),
-    path('like/<slug:slug>/', views.BlogPostLike.as_view(), name='post_likes'),
-
-
+    path('<slug:slug>/like/', like_post, name='post_likes'),
 ]
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
-from .views import HomeView
+from .views import BlogPostList, like_post_home
+from . import views
+
 
 urlpatterns = [
-     path('', HomeView.as_view(), name='index'),
+     # path('', HomeView.as_view(), name='index'),
+     path('', views.BlogPostList.as_view(), name='index'),
+     path('<slug:slug>/like/', like_post_home, name='post_likes'),
 
 ]
 
