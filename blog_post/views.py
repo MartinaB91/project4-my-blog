9ede6dedf5dfbc58526f3eebea493a274ac6a539
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-16 18:16:09 +0000

    refactor: Clean up functions/methods in blog post view

--- blog_post/views.py
+++ blog_post/views.py
@@ -1,9 +1,11 @@
+from datetime import datetime
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.urls import reverse
 from django.http import HttpResponse
-from django.views.generic import DetailView, ListView, View
+from django.views.generic import DetailView, ListView
 from django.views import generic
-from .models import Post, Like
+from .models import Post, Like, Comment
+from .forms import CommentForm
 
 
 class BlogPostDetail(generic.DetailView):
@@ -11,37 +13,36 @@ class BlogPostDetail(generic.DetailView):
     def get(self, request, slug, *args, **kwargs):
         template_name = 'blog_post.html'
         queryset = Post.objects.filter(published=1).order_by('-created_on')
-
         post = get_object_or_404(queryset, slug=slug)
+
         liked = False
         if post.likes.filter(id=request.user.id).exists():
             liked = True
 
+        context = {
+            'post': post,
+            'liked': liked,
+        }
+
         return render(
             request,
             "blog_post.html",
-            {
-                'post': post,
-                'liked': liked,
-            },
+            context,
         )
 
 def like_post(request, slug):
-    user = request.user
     if request.method == 'POST':
-        post_id = request.POST.get('post_id')
-        post_obj = get_object_or_404(Post, id=post_id)
+        post_obj = get_object_or_404(Post, id=request.POST.get('post_id'))
 
         if post_obj.likes.filter(id=request.user.id).exists():
-            post_obj.likes.remove(user.id)
+            post_obj.likes.remove(request.user.id)
             post_obj.number_of_likes -= 1
             post_obj.save()
 
         else:
-            post_obj.likes.add(user.id)
+            post_obj.likes.add(request.user.id)
             post_obj.number_of_likes += 1
             post_obj.save()
 
     return HttpResponse(post_obj.number_of_likes)
-
-
+  
