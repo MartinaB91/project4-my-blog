fed80640adf75e133242b247ea2109e4d46a606d
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-17 17:21:51 +0000

    feat: Add path for create post

--- blog_post/urls.py
+++ blog_post/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path, re_path
-from .views import BlogPostDetail, like_post
+from .views import BlogPostDetail, like_post, CreatePost
 from . import views
 
 
 urlpatterns = [
     # path('', views.BlogPostList.as_view(), name='index'),
+    path('create_post/', views.CreatePost.as_view(), name='create_post'),
     path('<slug:slug>/', views.BlogPostDetail.as_view(), name='blog_post'),
     path('<slug:slug>/like/', like_post, name='post_likes'),
+    
+    
 ]
