2811dae30ec872546d855735098ee0a6fdb7f3e6
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-18 07:56:32 +0000

    feat: Add path and form for create comment

--- blog_post/forms.py
+++ blog_post/forms.py
@@ -2,6 +2,11 @@ from .models import Comment, Post
 from django import forms
 
 
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ('content', )
+
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
--- blog_post/urls.py
+++ blog_post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, re_path
-from .views import BlogPostDetail, like_post, CreatePost
+from .views import BlogPostDetail, like_post, CreatePost, CreateComment
 from . import views
 
 
@@ -7,7 +7,7 @@ urlpatterns = [
     # path('', views.BlogPostList.as_view(), name='index'),
     path('create_post/', views.CreatePost.as_view(), name='create_post'),
     path('<slug:slug>/', views.BlogPostDetail.as_view(), name='blog_post'),
-    path('<slug:slug>/like/', like_post, name='post_likes'),
-    
+    path('<slug:slug>/like/', like_post, name='post_likes'),     
+    path('<slug:slug>/create_comment/', views.CreateComment.as_view(), name='create_comment'),
     
 ]
