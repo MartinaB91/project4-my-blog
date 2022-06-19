314df6fff4b637a5d0559da157a0bf623fdef459
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-19 18:20:42 +0000

    refactor: Changed nav search-btn from toggle to toggle-submit

--- static/js/base.js
+++ static/js/base.js
@@ -1,11 +1,20 @@
 // When user click search button, search field will be displayed.
 $(document).ready(function(){
+  
     $("#btn-search").click(function(){
+      
       if ($("#textbox-search").hasClass('collapse')){
         $("#textbox-search").removeClass('collapse');
+     
+
       }
       else {
-        $("#textbox-search").addClass('collapse');
+        $("#btn-search").prop("type", "submit");
       }
+      
     });
+    return false;
+
   });
+
+
