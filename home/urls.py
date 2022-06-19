c3ba8e2c4c4e1eaa8576ab6bcd60dc4aa19348ca
MartinaB91 <martinabjorlin@hotmail.com>
2022-06-19 15:07:39 +0000

    feat: Add functionality and html for presenting all posts from selected category

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import BlogPostList, like_post_home
+from .views import BlogPostList, like_post_home, CategoryPostList
 from . import views
 
 
@@ -7,7 +7,7 @@ urlpatterns = [
      # path('', HomeView.as_view(), name='index'),
      path('', views.BlogPostList.as_view(), name='index'),
      path('<slug:slug>/like/', like_post_home, name='post_likes'),
-
+     path('category/<slug:slug>/', views.CategoryPostList.as_view(), name='post_by_category'),
 ]
 
 
--- home/views.py
+++ home/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.views.generic import ListView, DetailView
-from blog_post.models import Post
+from blog_post.models import Post, Category
 from django.views import generic
 from django.http import HttpResponse
 
@@ -49,3 +49,28 @@ def like_post_home(request, slug):
             post_obj.save()
 
     return HttpResponse(post_obj.number_of_likes)
+
+class CategoryPostList(generic.ListView):
+    """ 
+     Used for sorting out selected category on page for a specifik category.
+     """
+    model = Post
+    template_name = 'posts_by_category.html'
+    context_object_name = 'posts_by_category'
+    
+    def get_queryset(self):
+        all_published_posts = Post.objects.filter(published=1).order_by('-created_on')
+        categories = Category.objects.all()
+
+        # Todo: Try to do better, split string on '/', category is second from last index.
+        wsgiRequestSplitted = str(self.request).split('/')  
+        # 'un-slugify' the string, in template acutal title is used so we replace '-' with ' '
+        selected_category = wsgiRequestSplitted[len(wsgiRequestSplitted)-2].replace('-', ' ')
+
+        queryset = {
+            'all_published_posts': all_published_posts,
+            'categories': categories,
+            'selected_category': selected_category
+        }
+
+        return queryset
