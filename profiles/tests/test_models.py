1a612e792934508ff4328ca7f74528a779dd13a3
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-05 17:47:31 +0000

    test: Started to add test for user and profile model, profile app

---
+++ profiles/tests/test_models.py
@@ -0,0 +1,40 @@
+import pytest
+from django.contrib.auth.models import User
+from profiles.models import Profile
+
+# Creating a user to be able to do other tests.
+
+def create_test_user(test_username):
+    user = User.objects.create(
+    username=test_username,
+    )
+    return user
+
+def create_test_profile(username, test_first_name, test_last_name):#, test_img, test_email):
+    user = create_test_user(username)
+    profile = Profile.objects.create(
+        user = user,
+        first_name = test_first_name,
+        last_name = test_last_name,
+        # profile_image = test_img,
+        # email = test_email,
+    )
+    
+# Tests if user is created as expected
+@pytest.mark.django_db
+def test_user_create():
+    test_user = create_test_user("AdaLovelace")
+    assert test_user.username == "AdaLovelace"
+
+# @pytest.mark.django_db
+# def test_profile_create():
+#     test_profile = create_test_profile('MartinaB', 'Martina', 'Bjorre')
+#     # assert test_profile.username == 'MartinaB'
+#     assert test_profile.first_name == 'Martina'
+#     assert test_profile.last_name == 'Bjorre'
+
+   
+
+
+
+
