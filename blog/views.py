09830ac9e8e77ad4fb9231bd438ac6997e484748
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-06 12:10:58 +0000

    style: Removed unused code from blog views

--- blog/views.py
+++ blog/views.py
@@ -1,15 +1,5 @@
-from django.shortcuts import render
 from blog_post.models import Post, Category
 from django.views.generic import ListView
-from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-# class MyBlogPostList(LoginRequiredMixin, ListView):
-#     """
-#      Used for showing  users posts on "my blog page"
-#      """
-#     model = Post
-#     template_name = 'my_blog.html'
 
 
 class MyBlogPostList(ListView):
