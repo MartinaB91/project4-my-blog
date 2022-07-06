70a38b0c886fe4034590befa9d87a1e2c0c79bfe
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-06 11:53:53 +0000

    style: Formatted blog views with Black

--- blog/views.py
+++ blog/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # class MyBlogPostList(LoginRequiredMixin, ListView):
-#     """ 
+#     """
 #      Used for showing  users posts on "my blog page"
 #      """
 #     model = Post
@@ -13,32 +13,31 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class MyBlogPostList(ListView):
-    """ 
-     Used for showing  users posts on "my blog page". 
-     Both published and unpublished.
-     """
+    """
+    Used for showing  users posts on "my blog page".
+    Both published and unpublished.
+    """
+
     model = Post
-    template_name = 'my_blog.html'
+    template_name = "my_blog.html"
     paginate_by = 6
 
     # Inspiration from:
     # https://stackoverflow.com/questions/61022964/how-do-i-access-the-context-data-in-the-template
     def get_context_data(self, *args, **kwargs):
         context = super(MyBlogPostList, self).get_context_data(**kwargs)
-        context['category'] = Category.objects.all()
+        context["category"] = Category.objects.all()
 
         return context
-        
 
     def get_queryset(self):
-        all_published_posts = Post.objects.order_by('-created_on')
+        all_published_posts = Post.objects.order_by("-created_on")
 
         for post in all_published_posts:
             post.liked = False
             if post.likes.filter(id=self.request.user.profile.id).exists():
                 post.liked = True
 
-        queryset = all_published_posts 
+        queryset = all_published_posts
 
         return queryset
-
