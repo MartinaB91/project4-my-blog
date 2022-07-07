5adb4ba9de5ca7df688c2d498a11a0304b825c62
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-07 15:07:34 +0000

    test: Created test for home view status code

---
+++ home/tests/test_views.py
@@ -0,0 +1,9 @@
+import pytest
+from django.urls import reverse
+
+# Tests that home view return expected status code
+@pytest.mark.django_db
+def test_home_view(client):
+    url = reverse('index')
+    response = client.get(url)
+    assert response.status_code == 200
