376545bd9539ced27fd4c16b3904330698167e45
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-13 19:46:03 +0000

    test: Refactored functions. Removed parameters

--- blog/tests/tests_views.py
+++ blog/tests/tests_views.py
@@ -6,27 +6,32 @@ from django.urls import reverse
 from blog_post.models import Post
 from blog.views import MyBlogPostList
 from datetime import date
-from blog_post.tests.tests_models import TestCategory
 from django.test import RequestFactory
-from tests.tests_utils import TestUserUtils
+from tests.tests_utils import TestUserUtils, TestCategoryUtils
+from django.test import TestCase
 
 
-# Tests that my blog list view return expected status code
 @pytest.mark.django_db
-def test_my_blog_list_view_response(client, django_user_model):
-    TestUserUtils.get_forced_login_test_user(client, django_user_model)
+def test_my_blog_list_view_response(client):
+    """
+    Tests that my blog list view return expected status code 200
+    """
+    TestUserUtils.get_forced_login_test_user(client)
     url = reverse('my_blog')
     response = client.get(url)
     assert response.status_code == 200
 
 
 @pytest.mark.django_db
-def test_view_returns_my_blog_context(client, django_user_model):
-    # Create a post that we can compare to what view returns.
-    user = get_forced_login_test_user(client, django_user_model)
-    category = TestCategory.create_test_category(
+def test_view_returns_my_blog_context(client):
+    """
+    Tests context data response in my blog view.
+    """
+    user = TestUserUtils.get_forced_login_test_user(client)
+    category = TestCategoryUtils.create_test_category(
         'Test category', 'test-category'
         )
+    # Create a post that we can compare to what view returns.
     Post.objects.create(
                 title='New post',
                 slug='new-post',
@@ -42,19 +47,20 @@ def test_view_returns_my_blog_context(client, django_user_model):
 
     response = client.get(reverse('my_blog'))
 
-    # Check context data frow view.
-    # A post created by user is by default not published.
-    assert response.context['has_not_published_posts'] is True
+    assert response.context['has_not_published_posts'] is True   # A post created by user is by default not published.
     assert response.context['has_published_posts'] is False
     assert response.context['category'].count() == 1
     assert response.context['category'].first() == category
 
 
 @pytest.mark.django_db
-def test_view_returns_my_blog_queryset(client, django_user_model):
+def test_view_returns_my_blog_queryset(client):
+    """
+    Test returned content of queryset
+    """
     # Create a post that we can compare to what view returns.
-    user = get_forced_login_test_user(client, django_user_model)
-    category = TestCategory.create_test_category(
+    user = TestUserUtils.get_forced_login_test_user(client)
+    category = TestCategoryUtils.create_test_category(
         'Test category', 'test-category'
         )
     post = Post.objects.create(
