eb5c479f692bd5e63aefd8da5f4976f5af4bb5a8
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-13 19:43:32 +0000

    test: Add tesst for profile model

--- profiles/tests/test_models.py
+++ profiles/tests/test_models.py
@@ -1,12 +1,24 @@
 import pytest
 from tests.tests_utils import TestUserUtils
+from profiles.models import Profile
 
-    
-# Tests if user is created as expected
 @pytest.mark.django_db
 def test_user_create():
-    test_user = TestUserUtils.create_test_user("AdaLovelace")
+    """ 
+    Tests if user is created
+    """
+    test_user = TestUserUtils.create_test_user("AdaLovelace", "password123")
     assert test_user.username == "AdaLovelace"
+
+
+@pytest.mark.django_db
+def test_profile_create_when_user_created():
+    """ 
+    Test if profile is created when user is. 
+    """ 
+    assert Profile.objects.all().count() == 0 # No profiles should exist
+    TestUserUtils.create_test_user("Blogger", "password123")
+    assert Profile.objects.all().count() == 1 # After creating a user 1 profile should exist
 
 
 
