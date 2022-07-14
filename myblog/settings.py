e9b247d4d1221329014bb204ffdbe1487b745fdc
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-14 13:56:41 +0000

    chore: Fixed prev misstake when reading development config vars

--- myblog/settings.py
+++ myblog/settings.py
@@ -17,7 +17,7 @@ if os.path.exists('env.py'):
     import env
 
 
-development = os.environ['DEVELOPMENT', False]
+development = os.environ('DEVELOPMENT', False)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
