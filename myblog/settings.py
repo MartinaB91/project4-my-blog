00f8f7248d27345dd88230b3a68b63811d9d1bb8
MartinaB91 <martinabjorlin@hotmail.com>
2022-07-14 13:52:26 +0000

    chore: Changed back to read var development from heroku config vars

--- myblog/settings.py
+++ myblog/settings.py
@@ -16,8 +16,8 @@ import dj_database_url
 if os.path.exists('env.py'):
     import env
 
-development = False
-# development = os.environ['DEVELOPMENT']
+
+development = os.environ['DEVELOPMENT', False]
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
